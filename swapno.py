7219a942cd2b5b5527208945e44cbe477bed2e2b
Vikram Ramavath <101108868+Ramavath-vikram@users.noreply.github.com>
2025-02-25 11:18:52 +0530

    Update swapno.py

--- swapno.py
+++ swapno.py
@@ -5,3 +5,10 @@ a=b
 b=temp
 print('The value of a after swapping: {}'.format(a))
 print('The value of b after swapping: {}'.format(b))
+
+
+#swapping without using third variable
+a=5
+b=10
+a,b=b,a
+print("a",a,"b",b)
